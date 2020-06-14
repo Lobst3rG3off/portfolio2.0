72506c8a515e79641e72cc8da5ddf9d723f76c6b
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-06-14 16:03:01 -0400

    reworking javascript and styles

--- guts.js
+++ guts.js
@@ -1,3 +1,64 @@
+// scroll menu
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 300) {
+      $('nav').addClass('fixed-header');
+      $('nav div').addClass('visible-title');
+  }
+  else {
+      $('nav').removeClass('fixed-header');
+      $('nav div').removeClass('visible-title');
+  }
+});
+// project viewer
+
+const cardButtons = document.querySelectorAll('.card button');
+const modalInner = document.querySelector('.modal-inner');
+const modalOuter = document.querySelector('.modal-outer');
+
+function handleCardButtonCLick(event) {
+    const button = event.currentTarget;
+    const card = button.closest('.card');
+    //  grab the image source
+    const imgSrc = card.querySelector('img').src;
+    const desc = card.dataset.description;
+    const name = card.querySelector('h2').textContent;
+    //  populate the modal with the new info
+    modalInner.innerHTML = `
+<img width="600" height="600" src="${imgSrc.replace(
+        '200',
+        '600'
+    )}" alt="${name}">
+<p>${desc}</p>
+`;
+    // show the modal
+    modalOuter.classList.add('open');
+}
+
+cardButtons.forEach(button =>
+    button.addEventListener('click', handleCardButtonCLick)
+);
+
+function closeModal() {
+    modalOuter.classList.remove('open');
+}
+
+modalOuter.addEventListener('click', function (event) {
+    const isOutside = !event.target.closest('.modal-inner');
+    console.log(isOutside);
+    if (isOutside) {
+        closeModal();
+    }
+});
+
+window.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+        closeModal();
+    }
+});
+
+
+
+
 const projectName = "random-quote-machine";
 localStorage.setItem('example_project', 'Random Quote Machine');
 let quotesData;
@@ -111,3 +172,6 @@ $(document).ready(function () {
     }
   });
 });
+
+
+
